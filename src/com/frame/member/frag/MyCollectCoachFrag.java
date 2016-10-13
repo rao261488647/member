5ce18957aa3acdbf73cabaf3225ab8a8e666cb69
ron <rao261488647@126.com>
2016-10-13 18:21:19 +0800

    我的收藏 教练展示

--- src/com/frame/member/frag/MyCollectCoachFrag.java
+++ src/com/frame/member/frag/MyCollectCoachFrag.java
@@ -4,35 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.frame.member.R;
 import com.frame.member.AppConstants.AppConstants;
 import com.frame.member.Parsers.BaseParser;
 import com.frame.member.Parsers.MyCollectCoachParser;
+import com.frame.member.Utils.CommonUtil;
 import com.frame.member.Utils.HttpRequest;
 import com.frame.member.Utils.HttpRequestImpl;
 import com.frame.member.Utils.SPUtils;
 import com.frame.member.activity.BaseActivity;
 import com.frame.member.activity.BaseActivity.DataCallback;
 import com.frame.member.activity.BaseActivity.RequestResult;
-import com.frame.member.adapters.SlideAdapter;
-import com.frame.member.adapters.SlideAdapter.DeleteItemListener;
+import com.frame.member.adapters.MyCollectCoachAdapter;
 import com.frame.member.bean.MyCollectBean.CollectCoach;
 import com.frame.member.bean.MyCollectBean.MyCollectCoachResult;
-import com.frame.member.slideItem.ListViewCompat;
-import com.frame.member.slideItem.MessageItem;
 import com.frame.member.widget.refreshlistview.PullToRefreshBase;
 import com.frame.member.widget.refreshlistview.PullToRefreshBase.Mode;
+import com.frame.member.widget.refreshlistview.PullToRefreshListView;
 import com.frame.member.widget.refreshlistview.PullToRefreshScrollView;
 
 /**
@@ -42,11 +38,9 @@ import com.frame.member.widget.refreshlistview.PullToRefreshScrollView;
  */
 public class MyCollectCoachFrag extends BaseFrag implements OnClickListener {
 
-	private SlideAdapter adapter;
-	private List<MessageItem> mMessageItems = new ArrayList<MessageItem>();
+	private MyCollectCoachAdapter adapter;
+	private PullToRefreshListView pullListView;
 	private List<CollectCoach> dataList = new ArrayList<CollectCoach>();
-	private ListViewCompat coachListView;
-	private PullToRefreshScrollView pullListView;
 	public static MyCollectCoachFrag newInstance(String title) {
 
 		MyCollectCoachFrag fragment = new MyCollectCoachFrag();
@@ -74,8 +68,7 @@ public class MyCollectCoachFrag extends BaseFrag implements OnClickListener {
 		return rootView;
 	}
 	private void initView(){
-		pullListView = (PullToRefreshScrollView)findViewById(R.id.my_collect_coach_sv);
-		coachListView = (ListViewCompat) findViewById(R.id.my_collect_coach_listView);
+		pullListView = (PullToRefreshListView)findViewById(R.id.my_collect_coach_listView);
 	}
 	/**
 	 * 点击监听事件绑定
@@ -91,6 +84,13 @@ public class MyCollectCoachFrag extends BaseFrag implements OnClickListener {
 	 */
 	private void initProgress(){
 		pullListView.setMode(Mode.BOTH);
+		pullListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//				startActivity(new Intent(getActivity(),ClassDetailActivity.class));
+			}
+		});
 		pullListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2() {
 
 			@Override
@@ -135,17 +135,11 @@ public class MyCollectCoachFrag extends BaseFrag implements OnClickListener {
 				if("200".equals(object.code)){
 					if(page == 1){
 						dataList.clear();
-						mMessageItems.clear();
 					}
 					if(object.collectCoachList != null && object.collectCoachList.size() > 0){
 						dataList.addAll(object.collectCoachList);
-						for(CollectCoach coach :  dataList){
-							MessageItem item = new MessageItem();
-							item.setCollectCoach(coach);
-							mMessageItems.add(item);
-						}
 						notifyAdapter();
-						setListViewHeight(coachListView);
+//						CommonUtil.setListViewHeight(coachListView);
 					}else{
 						showToast("没有更多数据！");
 					}
@@ -165,58 +159,14 @@ public class MyCollectCoachFrag extends BaseFrag implements OnClickListener {
 	 */
 	private void notifyAdapter() {
 		if(adapter == null){
-			adapter = new SlideAdapter(getActivity(),mMessageItems );
-			// 删除监听
-			adapter.setDeleteItemListener(new DeleteItemListener() {
-				@Override
-				public void deleteItem(View view, int position) {
-					mMessageItems.remove(position);
-					adapter.notifyDataSetChanged();
-					showToast("删除了第  " + position + " item");
-				}
-			});
+			adapter = new MyCollectCoachAdapter(getActivity(),dataList );
 			// 设置适配器
-			coachListView.setAdapter(adapter);
-			// 点击item事件
-			coachListView.setOnItemClickListener(new OnItemClickListener() {
-
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-					showToast("点击了第  " + position + " item");
-
-				}
-
-			});
+			pullListView.setAdapter(adapter);
 		}else{
 			adapter.notifyDataSetChanged();
 		}
 	}
 	
-	/**
-	 * 重新计算ListView的高度，解决ScrollView和ListView两个View都有滚动的效果，在嵌套使用时起冲突的问题
-	 * @param listView
-	 */
-	public void setListViewHeight(ListView listView) {  
-		  
-	    // 获取ListView对应的Adapter  
-	  
-	    ListAdapter listAdapter = listView.getAdapter();  
-	  
-	    if (listAdapter == null) {  
-	        return;  
-	    }  
-	    int totalHeight = 0;  
-	    for (int i = 0, len = listAdapter.getCount(); i < len; i++) { // listAdapter.getCount()返回数据项的数目  
-	        View listItem = listAdapter.getView(i, null, listView);  
-	        listItem.measure(0, 0); // 计算子项View 的宽高  
-	        totalHeight += listItem.getMeasuredHeight(); // 统计所有子项的总高度  
-	    }  
-	  
-	    ViewGroup.LayoutParams params = listView.getLayoutParams();  
-	    params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));  
-	    listView.setLayoutParams(params);  
-	}  
 	/**
 	 *点击事件判断 
 	 * @author Ron
