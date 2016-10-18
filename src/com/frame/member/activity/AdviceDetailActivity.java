0b984dd32110e9a30f14e0210807600caac7927d
yinqilong <yinqilong0377@163.com>
2016-10-19 07:25:09 +0800

    fix 社交-4 点击用户头像，不进入雪友空间

--- src/com/frame/member/activity/AdviceDetailActivity.java
+++ src/com/frame/member/activity/AdviceDetailActivity.java
@@ -22,6 +22,7 @@ import com.frame.member.bean.CoachCommentsResult;
 import com.frame.member.bean.CoachMembersCommentsResult;
 import com.frame.member.widget.RoundImageView;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.IBinder;
@@ -46,21 +47,21 @@ import android.widget.TextView;
 
 public class AdviceDetailActivity extends BaseActivity {
 
-	private ListView lv_member_comments,lv_coach_comments;
+	private ListView lv_member_comments, lv_coach_comments;
 	private CoachMemberCommentsAdapter mAdapter;
 	private CoachCommentsAdapter mCoachAdapter;
 	private PopupWindow mPop;
 	private View container_pop;
 	private ListView lv_booking_pop;
-	private ArrayAdapter<String> adapter_list ;
-	private ArrayAdapter<String> adapter_list_share ;
+	private ArrayAdapter<String> adapter_list;
+	private ArrayAdapter<String> adapter_list_share;
 	private View view_black_filter;
 	private List<String> list_str = new ArrayList<String>();
-	private ImageView iv_person_profile,iv_vedio_cover,iv_favour_num;
-	private TextView tv_name_person,tv_time_release,tv_member_level,tv_attention_button,
-					tv_content_advice_detail,tv_comments_num,tv_favour_num,tv_send_comments;
-	private LinearLayout ll_person_favor_profile; 
-	private String friendId,subjectId;
+	private ImageView iv_person_profile, iv_vedio_cover, iv_favour_num;
+	private TextView tv_name_person, tv_time_release, tv_member_level, tv_attention_button, tv_content_advice_detail,
+			tv_comments_num, tv_favour_num, tv_send_comments;
+	private LinearLayout ll_person_favor_profile;
+	private String friendId, subjectId;
 
 	@Override
 	protected void loadViewLayout() {
@@ -84,40 +85,40 @@ public class AdviceDetailActivity extends BaseActivity {
 		tv_favour_num = (TextView) findViewById(R.id.tv_favour_num);
 		tv_send_comments = (TextView) findViewById(R.id.tv_send_comments);
 		ll_person_favor_profile = (LinearLayout) findViewById(R.id.ll_person_favor_profile);
-		
+
 	}
 
 	@Override
 	protected void setListener() {
 		iv_title_right.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				showPopwindow(1);
 			}
 		});
 		tv_attention_button.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				toAttention(friendId, v);
 			}
 		});
 		iv_favour_num.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				toPaiseFriends(friendId, subjectId, v);
 			}
 		});
 		tv_send_comments.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				//TODO
+				// TODO
 			}
 		});
-		
+
 	}
 
 	@Override
@@ -125,7 +126,8 @@ public class AdviceDetailActivity extends BaseActivity {
 		tv_title.setText("动态详情");
 		iv_title_right.setImageResource(R.drawable.btn_more_normal3x);
 		iv_title_back.setImageResource(R.drawable.btn_back_normal);
-//		mAdapter = new CoachMemberCommentsAdapter(this, Arrays.asList("李欢", "李兴策", "福城阳", "周杰伦"));
+		// mAdapter = new CoachMemberCommentsAdapter(this, Arrays.asList("李欢",
+		// "李兴策", "福城阳", "周杰伦"));
 		lv_member_comments.setAdapter(mAdapter);
 		lv_member_comments.setOnItemClickListener(new OnItemClickListener() {
 
@@ -134,230 +136,225 @@ public class AdviceDetailActivity extends BaseActivity {
 				showPopwindow(0);
 			}
 		});
-		getWindow().setSoftInputMode( WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-		
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
 		view_black_filter.setAlpha(0.0f);
 		container_pop = getLayoutInflater().inflate(R.layout.item_booking_pop, null);
 		lv_booking_pop = (ListView) container_pop.findViewById(R.id.lv_booking_pop);
-		adapter_list = new ArrayAdapter<String>(
-				this, R.layout.item_pop_list,list_str);
+		adapter_list = new ArrayAdapter<String>(this, R.layout.item_pop_list, list_str);
 		lv_booking_pop.setAdapter(adapter_list);
-		
-		
+
 		getData();
 		getCoachComments();
 		getMembersComments();
 	}
-	
-	//点赞雪友
-		private void toPaiseFriends(String friendId,String subjectId,final View v){
-			int status;
-			if(R.drawable.zan_2x == (Integer)v.getTag()){
-				status = 0;
-				
-			}else{
-				status = 1;
-			}
-			BaseParser<BaseBean> parser = new NoBackParser();
-			HttpRequestImpl request = new HttpRequestImpl(
-					this, AppConstants.APP_SORT_STUDENT + "/praisefriend", parser);
-			request.addParam("memberUserId", 
-					(String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
-					.addParam("friendId", friendId)
-					.addParam("status", ""+status)
-					.addParam("subjectId", subjectId)
-					.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
-			DataCallback<BaseBean> callBack = new DataCallback<BaseBean>() {
 
-				@Override
-				public void processData(BaseBean object, RequestResult result) {
-					if(object != null){
-//						Toast.makeText(context, object.message, Toast.LENGTH_SHORT).show();
-						if(R.drawable.zan_2x == (Integer)v.getTag()){
-							((ImageView)v).setImageResource(R.drawable.un_zan_2x);
-							((ImageView)v).setTag(R.drawable.un_zan_2x);
-						}else{
-							((ImageView)v).setImageResource(R.drawable.zan_2x);
-							((ImageView)v).setTag(R.drawable.zan_2x);
-						}
-					}
-				}
-			};
-			getDataFromServer(request,false, callBack);
+	// 点赞雪友
+	private void toPaiseFriends(String friendId, String subjectId, final View v) {
+		int status;
+		if (R.drawable.zan_2x == (Integer) v.getTag()) {
+			status = 0;
+
+		} else {
+			status = 1;
 		}
-	
-	//关注\取消关注friends接口
-		private void toAttention(String friendId,final View v){
-			int status;
-			if("已关注".equals(((TextView)v).getText().toString())){
-				status = 0;
-			}else{
-				status = 1;
-			}
-			BaseParser<BaseBean> parser = new NoBackParser();
-			HttpRequestImpl request = new HttpRequestImpl(
-					this, AppConstants.APP_SORT_STUDENT + "/followfriend", parser);
-			request.addParam("memberUserId", 
-					(String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
-					.addParam("friendId", friendId)
-					.addParam("status", ""+status)
-					.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
-			DataCallback<BaseBean> callBack = new DataCallback<BaseBean>() {
+		BaseParser<BaseBean> parser = new NoBackParser();
+		HttpRequestImpl request = new HttpRequestImpl(this, AppConstants.APP_SORT_STUDENT + "/praisefriend", parser);
+		request.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
+				.addParam("friendId", friendId).addParam("status", "" + status).addParam("subjectId", subjectId)
+				.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
+		DataCallback<BaseBean> callBack = new DataCallback<BaseBean>() {
 
-				@Override
-				public void processData(BaseBean object, RequestResult result) {
-					if(object != null){
-						if("已关注".equals(((TextView)v).getText().toString())){
-							((TextView)v).setText("+关注");
-							((TextView)v).setBackgroundResource(R.drawable.shape_hollow_yellow);
-							((TextView)v).setTextColor(0xffe8ce39);
-						}else{
-							((TextView)v).setText("已关注");
-							((TextView)v).setBackgroundResource(R.drawable.shape_solid_yellow);
-							((TextView)v).setTextColor(0xff505050);
-						}
+			@Override
+			public void processData(BaseBean object, RequestResult result) {
+				if (object != null) {
+					// Toast.makeText(context, object.message,
+					// Toast.LENGTH_SHORT).show();
+					if (R.drawable.zan_2x == (Integer) v.getTag()) {
+						((ImageView) v).setImageResource(R.drawable.un_zan_2x);
+						((ImageView) v).setTag(R.drawable.un_zan_2x);
+					} else {
+						((ImageView) v).setImageResource(R.drawable.zan_2x);
+						((ImageView) v).setTag(R.drawable.zan_2x);
 					}
 				}
-			};
-			getDataFromServer(request,false, callBack);
-			
-		} 
-	//获取主数据
-	private void getData(){
+			}
+		};
+		getDataFromServer(request, false, callBack);
+	}
+
+	// 关注\取消关注friends接口
+	private void toAttention(String friendId, final View v) {
+		int status;
+		if ("已关注".equals(((TextView) v).getText().toString())) {
+			status = 0;
+		} else {
+			status = 1;
+		}
+		BaseParser<BaseBean> parser = new NoBackParser();
+		HttpRequestImpl request = new HttpRequestImpl(this, AppConstants.APP_SORT_STUDENT + "/followfriend", parser);
+		request.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
+				.addParam("friendId", friendId).addParam("status", "" + status)
+				.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
+		DataCallback<BaseBean> callBack = new DataCallback<BaseBean>() {
+
+			@Override
+			public void processData(BaseBean object, RequestResult result) {
+				if (object != null) {
+					if ("已关注".equals(((TextView) v).getText().toString())) {
+						((TextView) v).setText("+关注");
+						((TextView) v).setBackgroundResource(R.drawable.shape_hollow_yellow);
+						((TextView) v).setTextColor(0xffe8ce39);
+					} else {
+						((TextView) v).setText("已关注");
+						((TextView) v).setBackgroundResource(R.drawable.shape_solid_yellow);
+						((TextView) v).setTextColor(0xff505050);
+					}
+				}
+			}
+		};
+		getDataFromServer(request, false, callBack);
+
+	}
+
+	// 获取主数据
+	private void getData() {
 		BaseParser<AdviceDetailResult> parser = new AdviceDetailParser();
-		HttpRequestImpl request = new HttpRequestImpl(
-				this, AppConstants.APP_SORT_STUDENT+"/dynamicdetail", parser);
+		HttpRequestImpl request = new HttpRequestImpl(this, AppConstants.APP_SORT_STUDENT + "/dynamicdetail", parser);
 		request.addParam("subjectId", getIntent().getStringExtra("subjectId"))
-		.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
-		.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
+				.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
+				.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
 		getDataFromServer(request, callBack);
 	}
+
 	private DataCallback<AdviceDetailResult> callBack = new DataCallback<AdviceDetailResult>() {
 
 		@Override
-		public void processData(AdviceDetailResult object, RequestResult result) {
-			if(object != null){
+		public void processData(final AdviceDetailResult object, RequestResult result) {
+			if (object != null) {
 				friendId = object.friendId;
 				subjectId = object.subjectId;
 				TTApplication.getInstance().disPlayImageDef(object.user.appHeadThumbnail, iv_person_profile);
 				iv_person_profile.setOnClickListener(new OnClickListener() {
-					
+
 					@Override
 					public void onClick(View v) {
-						
+						Intent intent = new Intent(AdviceDetailActivity.this, FriendsSpaceActivity.class);
+						intent.putExtra(FriendsSpaceActivity.TAG_FRIEND_ID, object.user.friendId);
+						startActivity(intent);
 					}
 				});
 				TTApplication.getInstance().disPlayImageDef(object.videoPhoto, iv_vedio_cover);
 				tv_name_person.setText(object.user.memberName);
 				tv_time_release.setText(object.sendTime);
-				tv_member_level.setText("LV."+object.user.memberGrade);
-				if("0".equals(object.followAuthor)){
+				tv_member_level.setText("LV." + object.user.memberGrade);
+				if ("0".equals(object.followAuthor)) {
 					tv_attention_button.setText("+关注");
 					tv_attention_button.setBackgroundResource(R.drawable.shape_hollow_yellow);
 					tv_attention_button.setTextColor(0xffe8ce39);
-				}else{
+				} else {
 					tv_attention_button.setText("已关注");
 					tv_attention_button.setBackgroundResource(R.drawable.shape_solid_yellow);
 					tv_attention_button.setTextColor(0xff505050);
 				}
-				if("1".equals(object.praiseAuthor)){
+				if ("1".equals(object.praiseAuthor)) {
 					iv_favour_num.setImageResource(R.drawable.zan_2x);
 					iv_favour_num.setTag(R.drawable.zan_2x);
-				}else{
+				} else {
 					iv_favour_num.setImageResource(R.drawable.un_zan_2x);
 					iv_favour_num.setTag(R.drawable.un_zan_2x);
 				}
 				tv_content_advice_detail.setText(object.subjectName);
 				tv_comments_num.setText(object.commentNum);
 				tv_favour_num.setText(object.praiseNum);
-				
-				//动态加载点赞头像
-				if(object.list_friends != null && object.list_friends.size() > 0){
+
+				// 动态加载点赞头像
+				if (object.list_friends != null && object.list_friends.size() > 0) {
 					ll_person_favor_profile.removeAllViews();
 					int rank = 0;
-					for(Friends friend:object.list_friends){
+					for (Friends friend : object.list_friends) {
 						RoundImageView child = new RoundImageView(AdviceDetailActivity.this);
-						LayoutParams params = new LayoutParams(
-								CommonUtil.dip2px(AdviceDetailActivity.this, 35),
+						LayoutParams params = new LayoutParams(CommonUtil.dip2px(AdviceDetailActivity.this, 35),
 								CommonUtil.dip2px(AdviceDetailActivity.this, 35));
-						if(rank != 0){
+						if (rank != 0) {
 							params.leftMargin = CommonUtil.dip2px(AdviceDetailActivity.this, -5);
-							
+
 						}
 						child.setImageResource(R.drawable.profile_example_1);
-						if(!TextUtils.isEmpty(friend.appHeadThumbnail)){
-							TTApplication.getInstance().disPlayImageDef(
-									friend.appHeadThumbnail, child);
+						if (!TextUtils.isEmpty(friend.appHeadThumbnail)) {
+							TTApplication.getInstance().disPlayImageDef(friend.appHeadThumbnail, child);
 						}
-						ll_person_favor_profile.addView(child,params);
+						ll_person_favor_profile.addView(child, params);
 						rank++;
 					}
 				}
-				
+
 			}
 		}
 	};
+
 	// 获取教练评论数据
-	private void getCoachComments(){
+	private void getCoachComments() {
 		BaseParser<List<CoachCommentsResult>> parser = new CoachCommentsParser();
-		HttpRequestImpl request = new HttpRequestImpl(
-				this, AppConstants.APP_SORT_STUDENT+"/coachcomment", parser);
+		HttpRequestImpl request = new HttpRequestImpl(this, AppConstants.APP_SORT_STUDENT + "/coachcomment", parser);
 		request.addParam("subjectId", getIntent().getStringExtra("subjectId"))
 				.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
 				.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
 		getDataFromServer(request, callback1);
 	}
-	private DataCallback<List<CoachCommentsResult>> callback1 = 
-			new DataCallback<List<CoachCommentsResult>>() {
+
+	private DataCallback<List<CoachCommentsResult>> callback1 = new DataCallback<List<CoachCommentsResult>>() {
 
 		@Override
 		public void processData(List<CoachCommentsResult> object, RequestResult result) {
-			if(object != null){
+			if (object != null) {
 				notifyCoachAdapter(object);
 			}
 		}
-	}; 
-	//刷新教练评论的数据
-	private void notifyCoachAdapter(List<CoachCommentsResult> object){
-		if(mCoachAdapter == null){
-			mCoachAdapter = new CoachCommentsAdapter(AdviceDetailActivity.this, object,getIntent().getStringExtra("subjectId"));
+	};
+
+	// 刷新教练评论的数据
+	private void notifyCoachAdapter(List<CoachCommentsResult> object) {
+		if (mCoachAdapter == null) {
+			mCoachAdapter = new CoachCommentsAdapter(AdviceDetailActivity.this, object,
+					getIntent().getStringExtra("subjectId"));
 			lv_coach_comments.setAdapter(mCoachAdapter);
-		}else{
+		} else {
 			mCoachAdapter.notifyDataSetChanged();
 		}
 	}
-	
-	//获取学员评论数据
-	private void getMembersComments(){
+
+	// 获取学员评论数据
+	private void getMembersComments() {
 		BaseParser<List<CoachMembersCommentsResult>> parser = new CoachMembersCommentsParser();
-		HttpRequestImpl request = new HttpRequestImpl(
-				this, AppConstants.APP_SORT_STUDENT+"/studentcomment", parser);
+		HttpRequestImpl request = new HttpRequestImpl(this, AppConstants.APP_SORT_STUDENT + "/studentcomment", parser);
 		request.addParam("subjectId", getIntent().getStringExtra("subjectId"))
-		.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
-		.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
+				.addParam("memberUserId", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_MEMBERUSERID, ""))
+				.addParam("token", (String) SPUtils.getAppSpUtil().get(this, SPUtils.KEY_TOKEN, ""));
 		getDataFromServer(request, callback2);
 	}
-	private DataCallback<List<CoachMembersCommentsResult>> callback2 = 
-			new DataCallback<List<CoachMembersCommentsResult>>() {
+
+	private DataCallback<List<CoachMembersCommentsResult>> callback2 = new DataCallback<List<CoachMembersCommentsResult>>() {
 
 		@Override
 		public void processData(List<CoachMembersCommentsResult> object, RequestResult result) {
-			if(object != null){
+			if (object != null) {
 				notifyMembersAdapter(object);
 			}
 		}
-	}; 
-	//刷新学员评论的数据
-	private void notifyMembersAdapter(List<CoachMembersCommentsResult> object){
-		if(mAdapter == null){
+	};
+
+	// 刷新学员评论的数据
+	private void notifyMembersAdapter(List<CoachMembersCommentsResult> object) {
+		if (mAdapter == null) {
 			mAdapter = new CoachMemberCommentsAdapter(AdviceDetailActivity.this, object);
 			lv_member_comments.setAdapter(mAdapter);
-		}else{
+		} else {
 			mAdapter.notifyDataSetChanged();
 		}
 	}
-	
-	private void showPopwindow(int rank){
+
+	private void showPopwindow(int rank) {
 		list_str.clear();
 		switch (rank) {
 		case 0:
@@ -367,29 +364,27 @@ public class AdviceDetailActivity extends BaseActivity {
 			adapter_list.notifyDataSetChanged();
 			break;
 		case 1:
-//			list_str.add("分享");
+			// list_str.add("分享");
 			list_str.add("举报");
 			list_str.add("取消");
-			break;	
+			break;
 
 		}
-		if(mPop == null){
-			mPop = new PopupWindow(container_pop, 
-					LinearLayout.LayoutParams.MATCH_PARENT, 
-					LinearLayout.LayoutParams.WRAP_CONTENT,true);
+		if (mPop == null) {
+			mPop = new PopupWindow(container_pop, LinearLayout.LayoutParams.MATCH_PARENT,
+					LinearLayout.LayoutParams.WRAP_CONTENT, true);
 		}
-		mPop.setBackgroundDrawable(
-				new BitmapDrawable(getResources(),(Bitmap)null));
-//		mPop.setOutsideTouchable(true);
+		mPop.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+		// mPop.setOutsideTouchable(true);
 		mPop.setOnDismissListener(new OnDismissListener() {
-			
+
 			@Override
 			public void onDismiss() {
 				view_black_filter.setAlpha(0.0f);
-				
+
 			}
 		});
-//		mPop.showAsDropDown(ll_booking_item,0,0);
+		// mPop.showAsDropDown(ll_booking_item,0,0);
 		mPop.showAtLocation(view_black_filter, Gravity.BOTTOM, 0, 0);
 		view_black_filter.setAlpha(0.5f);
 	}
@@ -429,6 +424,5 @@ public class AdviceDetailActivity extends BaseActivity {
 			manager.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
 		}
 	}
-	
 
 }
