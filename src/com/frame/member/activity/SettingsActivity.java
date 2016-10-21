321b4c6d622b8c3442cb332538ed32cabc94906e
longqian1235 <497731042@qq.com>
2016-10-21 14:39:44 +0800

    个人设置修改

--- src/com/frame/member/activity/SettingsActivity.java
+++ src/com/frame/member/activity/SettingsActivity.java
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 package com.frame.member.activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+
 
 /**
  * 设置
@@ -25,6 +14,17 @@ import com.frame.member.Utils.DataCleanManager;
 import com.frame.member.Utils.HttpRequestImpl;
 import com.frame.member.Utils.SPUtils;
 import com.frame.member.bean.BaseBean;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SettingsActivity extends BaseActivity implements OnClickListener {
 	
@@ -200,55 +200,4 @@ public class SettingsActivity extends BaseActivity implements OnClickListener {
 	}
 
 }
-=======
-package com.frame.member.activity;
-import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 
-/**
- * 设置
- * Ron
- */
-import com.frame.member.R;
-
-public class SettingsActivity extends BaseActivity implements OnClickListener {
-	
-	
-	@Override
-	protected void loadViewLayout() {
-		setContentView(R.layout.activity_settings);
-	}
-
-	@Override
-	protected void findViewById() {
-	  	//lv_statemetns_list = (ListView) findViewById(R.id.lv_statements_list);
-		tv_title.setText("设置");
-		
-	}
-	
-	@Override
-	protected void setListener() {
-
-	}
-
-	@Override
-	protected void processLogic() {
-		iv_title_back.setVisibility(0);
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.my_text2:
-			Intent intent = new Intent(this, MyInfoActivity.class);
-			this.startActivity(intent);
-			break;
-
-		default:
-			break;
-		}
-	}
-}
->>>>>>> branch 'master' of https://github.com/rao261488647/member.git
